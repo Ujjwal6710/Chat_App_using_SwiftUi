5fa24a91923f794bbbec40715e4ac84d659b8424
Ujjwal6710 <110162525+Ujjwal6710@users.noreply.github.com>
2024-01-16 21:36:57 +0530

    Add files via upload

---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Major Project
+//
+//  Created by Ujjwal Chopra on 10/10/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
